94d152015b3ffced042b1ae3804b9f78d1a25935
AntonioBrivio <92523244+AntonioBrivio@users.noreply.github.com>
2024-04-17 13:29:26 -0300

    Menu do Restaurante

---
+++ Restaurante/app/src/main/java/com/example/restaurante/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.restaurante
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.restaurante.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+
+        val username = intent.extras?.getString("username")
+
+        if(!username.isNullOrEmpty()){
+            binding.textOla.setText("Olá $username")
+        }
+
+        binding.buttonFechar.setOnClickListener {
+            finishAffinity()
+        }
+
+
+        binding.checkPizza.setOnClickListener {
+            if(binding.checkPizza.isChecked()){
+                binding.editQuantidadePizza.setText("1")
+                binding.textPrecoPizza.visibility = View.VISIBLE
+            }else{
+                binding.editQuantidadePizza.setText("0")
+                binding.textPrecoPizza.visibility = View.GONE
+            }
+        }
+
+        binding.checkSalada.setOnClickListener {
+            if(binding.checkSalada.isChecked()){
+                binding.editQuantidadeSalada.setText("1")
+                binding.textPrecoSalada.visibility = View.VISIBLE
+            }else{
+                binding.editQuantidadeSalada.setText("0")
+                binding.textPrecoSalada.visibility = View.GONE
+            }
+        }
+
+        binding.checkHamburger.setOnClickListener {
+            if(binding.checkHamburger.isChecked()){
+                binding.editQuantidadeHamburguer.setText("1")
+                binding.textPrecoHamburguer.visibility = View.VISIBLE
+            }else{
+                binding.editQuantidadeHamburguer.setText("0")
+                binding.textPrecoHamburguer.visibility = View.GONE
+            }
+        }
+
+    }
+}
