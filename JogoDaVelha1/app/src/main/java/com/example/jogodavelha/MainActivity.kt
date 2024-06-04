9a7d61f4b6856e0812f5f38e6429882fa38e54b7
AntonioBrivio <92523244+AntonioBrivio@users.noreply.github.com>
2024-06-04 11:54:43 -0300

    Jogo da Velha Nova Versão

---
+++ JogoDaVelha1/app/src/main/java/com/example/jogodavelha/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.jogodavelha
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.jogodavelha.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
+    //Vetor bidimensional que representará o tabuleiro de jogo
+    val tabuleiro = arrayOf(
+        arrayOf("A", "B", "C"),
+        arrayOf("D", "E", "F"),
+        arrayOf("G", "H", "I")
+    )
+
+    //Qual o Jogador está jogando
+    var jogadorAtual = "X"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+    }
+
+    //Função associada com todos os botões @param view é o botão clicado
+    fun buttonClick(view: View) {
+        //O botão clicado é associado com uma constante
+        val buttonSelecionado = view as Button
+        //O texto do botão recebe o jogador atual
+        buttonSelecionado.text = jogadorAtual
+
+        //De acordo com o botão clicado, a posição da matriz receberá o Jogador
+        when(buttonSelecionado.id){
+            binding.buttonZero.id -> tabuleiro[0][0] = jogadorAtual
+            binding.buttonUm.id -> tabuleiro[0][1] = jogadorAtual
+            binding.buttonDois.id -> tabuleiro[0][2] = jogadorAtual
+            binding.buttonTres.id -> tabuleiro[1][0] = jogadorAtual
+            binding.buttonQuatro.id -> tabuleiro[1][1] = jogadorAtual
+            binding.buttonCinco.id -> tabuleiro[1][2] = jogadorAtual
+            binding.buttonSeis.id -> tabuleiro[2][0] = jogadorAtual
+            binding.buttonSete.id -> tabuleiro[2][1] = jogadorAtual
+            binding.buttonOito.id -> tabuleiro[2][2] = jogadorAtual
+        }
+
+        if(jogadorAtual.equals("X")) {
+            buttonSelecionado.setBackgroundColor(Color.BLUE)
+            jogadorAtual = "O"
+        }else{
+            buttonSelecionado.setBackgroundColor(Color.RED)
+            jogadorAtual = "X"
+        }
+        buttonSelecionado.isEnabled=false
+    }
+
+}
