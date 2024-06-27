a64ab3b46c3b6470574ab3c6ed36786919e0ae96
AntonioBrivio <92523244+AntonioBrivio@users.noreply.github.com>
2024-06-27 11:39:48 -0300

    Exemplos de Classes

---
+++ RestauranteNovo/app/src/main/java/com/example/restaurantenovo/model/FoodJava.java
@@ -0,0 +1,40 @@
+package com.example.restaurantenovo.model;
+
+public class FoodJava {
+    private int imagem;
+    private String nome;
+    private String descricao;
+    private String preco;
+
+    public int getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(int imagem) {
+        this.imagem = imagem;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+}
---
+++ RestauranteNovo/app/src/main/java/com/example/restaurantenovo/model/FoodKotlin.kt
@@ -0,0 +1,8 @@
+package com.example.restaurantenovo.model
+
+data class FoodKotlin(
+    var imagem: Int? = null,
+    var nome: String? = null,
+    var descricao: String? = null,
+    var preco: String? = null
+)
